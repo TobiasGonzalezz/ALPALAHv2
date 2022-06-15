d813a2a2f93aac5ba597f3d10a8ecc866407cf2f
tobiasgonzalez398 <tobygon988@gmail.com>
2022-06-15 13:30:20 -0300

    animacion mas lenta del logo, se posiciono el precio a la derecha, se hizo el qr shadow y se borro momentaneamente la suma js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,19 @@
-const prices = document.querySelectorAll('h3 span');
-const container__comidas = document.querySelectorAll('.container__item');
+// NO LO VAMOS A USAR POR AHORA
 
-let suma = 0;
 
-container__comidas.forEach((comida, i) => {
+// const prices = document.querySelectorAll('h3 span');
+// const container__comidas = document.querySelectorAll('.container__item');
 
-    comida.addEventListener('click', (event) => {
+// let suma = 0;
+
+// container__comidas.forEach((comida, i) => {
+
+//     comida.addEventListener('click', (event) => {
         
-        suma += parseInt(prices[i].innerText);
-        total.value = `Total: $${suma}`; //Se puede acceder al valor directamente desde su ID
-    })
-})
+//         suma += parseInt(prices[i].innerText);
+//         total.value = `Total: $${suma}`; //Se puede acceder al valor directamente desde su ID
+//     })
+// })
 
 // const total = document.querySelector('input');
 
